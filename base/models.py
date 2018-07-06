b60268b4f9e4329b7d858be70ffda0a61c2f47bc
ZohanHo <serduk.evgeniy@gmail.com>
2018-07-06 11:52:16 +0300

    06.07.2018

--- base/models.py
+++ base/models.py
@@ -7,3 +7,9 @@ class Test(models.Model):
     class Meta():
         verbose_name = "Тест"
         verbose_name_plural = "Тесты"
+
+    def __str__(self): # Даем строковое представление обьекта, а именно записи в базе
+        try:    # Если не возникнит ошибки в блоке try, он выполнится, иначе блок except
+            return "Имя пользователя: {}".format(self.name)    # Может быть ошибка если поле name не найдено, тогда except
+        except:
+            return "{} {}".format(self.id, self.pk)   # Id всегда есть, так же можно выводить пару полей
